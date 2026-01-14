df75f9e1f431bf1306eca95d9628938b6affedc0
Samuel <samuelguarapari@gmail.com>
2026-01-14 20:31:43 -0300

    Diversos

---
+++ opamenu-commons/OpaMenu.Commons/Api/Commons/ResponseBuilder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpaMenu.Commons.Api.Commons
+{
+    internal class ResponseBuilder
+    {
+    }
+}
